2084b6b1af07b4c8ddb96a977731896b57b54d25
Panagiotis Spanakis <t8200158@aueb.gr>
2024-05-13 23:31:11 +0300

    Update subject to be subject_id from asjcs (pending confirmation)

--- h5-index-small/works_issn_subject.sql
+++ h5-index-small/works_issn_subject.sql
@@ -1,6 +1,8 @@
 -- Create common electronic and print ISSN lookup
 
 CREATE TABLE rolap.works_issn_subject AS
-    SELECT doi, Coalesce(works.issn_print, works.issn_electronic) AS issn, work_subjects.name as subject
-    FROM works INNER JOIN work_subjects ON works.id = work_subjects.work_id
+    SELECT doi, Coalesce(works.issn_print, works.issn_electronic) AS issn, asjc_general_fields.id as subject
+    FROM works INNER JOIN works_asjcs ON works.id = works_asjcs.work_id
+               INNER JOIN asjcs ON asjcs.id = works_asjcs.asjc_id
+               INNER JOIN asjc_general_fields ON asjc_general_fields.id = asjcs.general_field_id
     WHERE issn is not null;
