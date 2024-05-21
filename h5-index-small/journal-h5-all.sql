7ffcba6edfd867b01d3729ce7c6cf7e3944536b4
Panagiotis Spanakis <t8200158@aueb.gr>
2024-05-21 20:35:49 +0300

    Change general field to subject field (more general) for subject

--- h5-index-small/journal-h5-all.sql
+++ h5-index-small/journal-h5-all.sql
@@ -7,12 +7,12 @@ CREATE INDEX IF NOT EXISTS journal_names_iss_print_idx
 CREATE INDEX IF NOT EXISTS journal_names_iss_eprint_idx
   ON journal_names(issn_eprint);
 
-CREATE INDEX IF NOT EXISTS asjc_general_fields_id_idx
-  ON asjc_general_fields(id);
+CREATE INDEX IF NOT EXISTS asjc_subject_areas_id_idx
+  ON asjc_subject_areas(id);
 
-SELECT title, asjc_general_fields.name, h5_index FROM rolap.issn_subject_h5 LEFT JOIN journal_names
+SELECT title, asjc_subject_areas.name, h5_index FROM rolap.issn_subject_h5 LEFT JOIN journal_names
   ON rolap.issn_subject_h5.issn = journal_names.issn_print
     OR rolap.issn_subject_h5.issn = journal_names.issn_eprint
-  LEFT JOIN asjc_general_fields ON subject = asjc_general_fields.id
+  LEFT JOIN asjc_subject_areas ON subject = asjc_subject_areas.id
   WHERE h5_index is not null AND title is not null
   ORDER BY h5_index DESC, title ASC;
